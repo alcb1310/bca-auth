6686c4cf8131cb43af4031251e1b0839e4321da3
Andres Court <andres@andrescourt.com>
2025-10-11 23:08:19 -0500

    Go chi router initialization

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var (
+	port = os.Getenv("PORT")
+)
 
 func main() {
-	fmt.Println("Hello, World with air!")
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	fmt.Println("Listening on port " + port)
+	_ = http.ListenAndServe(":"+port, r)
 }
