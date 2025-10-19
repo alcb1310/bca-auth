30510e8e44ed6d4c5a221cf034a4fdda6ac5d7a4
Andres Court <andres@andrescourt.com>
2025-10-19 12:10:53 -0500

    added logging to the project route

--- internal/database/project.go
+++ internal/database/project.go
@@ -1,21 +1,26 @@
 package database
 
-import "github.com/alcb1310/bca-auth/internal/types"
+import (
+	"log/slog"
+
+	"github.com/alcb1310/bca-auth/internal/types"
+)
 
 func (s *service) GetAllProjects() ([]types.Project, error) {
-	sql := "SELECT id, name, is_active, gross_area, net_area FROM projects order by name"
+	var projects = []types.Project{}
+	sql := "SELECT id, name, is_active, gross_area, net_area FROM project order by name"
 
 	rows, err := s.db.Query(sql)
 	if err != nil {
+		slog.Error("Error executing query", "err", err)
 		return nil, err
 	}
 	defer func() { _ = rows.Close() }()
 
-	var projects []types.Project
 	for rows.Next() {
 		var p types.Project
-		err = rows.Scan(&p.ID, &p.Name, &p.IsActive, &p.GrossArea, &p.NetArea)
-		if err != nil {
+		if err = rows.Scan(&p.ID, &p.Name, &p.IsActive, &p.GrossArea, &p.NetArea); err != nil {
+			slog.Error("Error scanning row", "err", err)
 			return nil, err
 		}
 		projects = append(projects, p)
--- internal/server/projects.go
+++ internal/server/projects.go
@@ -2,13 +2,18 @@ package server
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 )
 
 func (s *Server) getAllProjects(w http.ResponseWriter, r *http.Request) {
 	projects, err := s.DB.GetAllProjects()
+	slog.Info("getAllProjects", "user from context", r.Context().Value("user"))
+	slog.Info("getAllProjects", "user from Server", s.User)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		slog.Error("getAllProjects", "err", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
 		return
 	}
 
